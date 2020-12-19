0a8a00b4e47f7e94c60736b17531da56c1161737
Elon Park <dev.sungwoon@gmail.com>
2020-12-20 04:11:08 +0900

    Support Carthage

---
+++ ReactorKit/ReactorKit.h
@@ -0,0 +1,19 @@
+//
+//  ReactorKit.h
+//  ReactorKit
+//
+//  Created by Elon on 2020/12/20.
+//  Copyright © 2020 ReactorKit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ReactorKit.
+FOUNDATION_EXPORT double ReactorKitVersionNumber;
+
+//! Project version string for ReactorKit.
+FOUNDATION_EXPORT const unsigned char ReactorKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactorKit/PublicHeader.h>
+
+
