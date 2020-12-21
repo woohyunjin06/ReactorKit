22afe369fd0a2fccf1dde9ba6b0327c9a253a4e2
Elon Park <dev.sungwoon@gmail.com>
2020-12-21 18:22:39 +0900

    Fix ReactorKitRuntime

--- ReactorKit/ReactorKit.h
+++ ReactorKit/ReactorKit.h
@@ -14,6 +14,4 @@ FOUNDATION_EXPORT double ReactorKitVersionNumber;
 //! Project version string for ReactorKit.
 FOUNDATION_EXPORT const unsigned char ReactorKitVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <ReactorKit/PublicHeader.h>
-
-
+#import "ReactorKitRuntime.h"
